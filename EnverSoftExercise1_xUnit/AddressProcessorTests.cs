f32d51d7b7fa75de4e5e3025ef9b15c59240e246
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2025-04-28 17:55:52 +0200

    test: configured xUnit project with project reference and verified initial test setup

---
+++ EnverSoftExercise1_xUnit/AddressProcessorTests.cs
@@ -0,0 +1,34 @@
+﻿using CsvProcessorApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EnverSoftExercise1_xUnit
+{
+    public class AddressProcessorTests
+    {
+        [Fact]
+        public void GetSortedAddresses_ShouldSortByStreetName()
+        {
+            // Arrange
+            var processor = new AddressProcessor();
+            var addresses = new List<string>
+            {
+                "31 Clifton Rd",
+                "12 Acton St",
+                "22 Jones Rd"
+            };
+
+            // Act
+            var result = processor.GetSortedAddresses(addresses);
+
+            // Assert
+            Assert.Equal("12 Acton St", result[0]);
+            Assert.Equal("31 Clifton Rd", result[1]);
+            Assert.Equal("22 Jones Rd", result[2]);
+        }
+    }
+}
--- EnverSoftExercise1_xUnit/NameProcessorTests.cs
+++ EnverSoftExercise1_xUnit/NameProcessorTests.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using CsvProcessorApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace EnverSoftExercise1_xUnit
 {
-    internal class NameProcessorTests
+    public class NameProcessorTests
     {
+        [Fact]
+        public void GetNameFrequencies_ShouldReturnCorrectFrequencies()
+        {
+            // Arrange
+            var processor = new NameProcessor();
+            var names = new List<string> { "Clive", "James", "Clive", "Graham", "Graham", "James", "Jimmy", "John" };
+
+            // Act
+            var result = processor.GetNameFrequencies(names);
+
+            // Assert
+            Assert.Equal("Clive, 2", result[0]);
+            Assert.Equal("Graham, 2", result[1]);
+            Assert.Equal("James, 2", result[2]);
+            Assert.Contains("Jimmy, 1", result);
+            Assert.Contains("John, 1", result);
+        }  
     }
 }
