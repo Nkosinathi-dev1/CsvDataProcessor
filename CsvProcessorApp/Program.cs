46d3696932d6dfe4ecf196e284e809ffd4bc9052
Nkosinathi-dev1 <nkosinathi.ngele.dev@gmail.com>
2025-04-28 14:08:14 +0200

    Set up CsvProcessorApp

---
+++ CsvProcessorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CsvProcessorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set uping CsvProcessorApp");
+        }
+    }
+}
